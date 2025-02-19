c5e142e9478d50585755c1b890c61cff48c1269e
Joseff <iosif.yusim@gmail.com>
2025-02-19 02:48:40 +0200

    Fixed ESLint issues

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -17,37 +17,37 @@ test('should return status code 200 or handle error appropriately', async () => 
     }
 });
 test('should return expected data properties', async () => {
-	let response;
-	try {
-	  response = await fetch(`${config.API_URL}/warehouses`);
-	} catch (error) {
-	  console.error('API request failed:', error);
-	}
+    let response;
+    try {
+      response = await fetch(`${config.API_URL}/warehouses`);
+    } catch (error) {
+      console.error('API request failed:', error);
+    }
   
-	if (response) {
-	  const data = await response.json(); // Парсим JSON-ответ
-	  // Проверяем, что данные содержат нужные свойства
-	  expect(data[0]).toHaveProperty('name'); // Проверяем, что первый элемент массива содержит свойство 'name'
-	  expect(data[0].workingHours).toHaveProperty('start'); // Проверяем, что 'workingHours' содержит свойство 'start'
-	  expect(data[0].workingHours).toHaveProperty('end'); // Проверяем, что 'workingHours' содержит свойство 'end'
-	} else {
-	  expect(response).not.toBeNull(); // Если ответа нет, тест не пройден
-	}
+    if (response) {
+      const data = await response.json(); // Парсим JSON-ответ
+      // Проверяем, что данные содержат нужные свойства
+      expect(data[0]).toHaveProperty('name'); // Проверяем, что первый элемент массива содержит свойство 'name'
+      expect(data[0].workingHours).toHaveProperty('start'); // Проверяем, что 'workingHours' содержит свойство 'start'
+      expect(data[0].workingHours).toHaveProperty('end'); // Проверяем, что 'workingHours' содержит свойство 'end'
+    } else {
+      expect(response).not.toBeNull(); // Если ответа нет, тест не пройден
+    }
   });
   test('should return a non-empty list of warehouses', async () => {
-	let response;
-	try {
-	  response = await fetch(`${config.API_URL}/warehouses`);
-	} catch (error) {
-	  console.error('API request failed:', error);
-	}
+    let response;
+    try {
+      response = await fetch(`${config.API_URL}/warehouses`);
+    } catch (error) {
+      console.error('API request failed:', error);
+    }
   
-	if (response) {
-	  const data = await response.json(); // Парсим JSON-ответ
-	  // Проверяем, что список складов не пуст
-	  expect(data.length).toBeGreaterThan(0); // Проверка, что длина массива больше 0
-	} else {
-	  expect(response).not.toBeNull(); // Если ответа нет, тест не пройден
-	}
+    if (response) {
+      const data = await response.json(); // Парсим JSON-ответ
+      // Проверяем, что список складов не пуст
+      expect(data.length).toBeGreaterThan(0); // Проверка, что длина массива больше 0
+    } else {
+      expect(response).not.toBeNull(); // Если ответа нет, тест не пройден
+    }
   });
   
