145bb789ea2d5012ce544548097fe6e9afec5faf
Joseff <iosif.yusim@gmail.com>
2025-02-19 13:15:14 +0200

    Fixed config.js to use CommonJS

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
-export default {
+module.exports = {
     API_URL: "https://cnt-f9377861-ba5c-4d4d-bcc5-99bc29ac4e76.containerhub.tripleten-services.com/api/v1"
 };
