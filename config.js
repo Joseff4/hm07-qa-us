9995f80884fa7bee7d96a5a0cbd2ac3a858ae961
Joseff <iosif.yusim@gmail.com>
2025-02-19 02:52:55 +0200

    Updated API URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-cae18d7b-3cc7-4803-affb-74ddcea536f8.containerhub.tripleten-services.com/api/v1"
+    API_URL: "https://cnt-a44359bd-31b4-44c1-8ea7-20997cefa840.containerhub.tripleten-services.com/api/v1"
 };
